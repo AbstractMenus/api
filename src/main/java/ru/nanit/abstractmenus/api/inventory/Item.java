a8831d7769ec7b6e1e60328cedc97ff0f680a233
Nanit <nanitmix@gmail.com>
2021-10-11 14:31:56 +0300

    (Experimental) Changed Item class to iterface. Make common ItemProperty api

--- src/main/java/ru/nanit/abstractmenus/api/inventory/Item.java
+++ src/main/java/ru/nanit/abstractmenus/api/inventory/Item.java
@@ -2,101 +2,34 @@ package ru.nanit.abstractmenus.api.inventory;
 
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.*;
-import ru.nanit.abstractmenus.api.Logger;
 import ru.nanit.abstractmenus.api.inventory.property.ItemProperty;
-import ru.nanit.abstractmenus.api.inventory.property.MaterialProperty;
-import ru.nanit.abstractmenus.api.inventory.property.SimpleProperty;
 
 import java.util.*;
 
 /**
  * Represent buildable ItemStack
  */
-public class Item implements Cloneable {
-
-    private Map<String, ItemProperty> properties;
-
-    public Item(){}
-
-    public Item(Map<String, ItemProperty> properties){
-        this.properties = properties;
-    }
-
-    public Item(Item item){
-        this(item.getProperties());
-    }
+public interface Item extends Cloneable {
 
     /**
      * Get properties if the item
      * @return List of item properties
      */
-    public Map<String, ItemProperty> getProperties(){
-        return properties;
-    }
+    Map<String, ItemProperty> getProperties();
 
     /**
      * Add some property for this item
      * @param key Property key
      * @param property Item property
      */
-    public void addProperty(String key, ItemProperty property){
-        if (properties == null) properties = new HashMap<>();
-        properties.put(key.toLowerCase(), property);
-    }
+    void addProperty(String key, ItemProperty property);
 
     /**
      * Remove property from item
      * @param key Key (name) of the property. This key you specify in item block in menu file
      * @return Removed property or null if property with specified key is not assigned to item
      */
-    public ItemProperty removeProperty(String key){
-        return properties.remove(key.toLowerCase());
-    }
-
-    /**
-     * Build ItemStack of this item
-     * @param player Player to correct replace all placeholders
-     * @param menu Menu in which this item exists. Might be null
-     * @return Built ItemStack object
-     */
-    public ItemStack build(Player player, Menu menu){
-        try{
-            ItemStack item = null;
-
-            // Apply material property first
-            for (ItemProperty property : properties.values()){
-                if (property instanceof MaterialProperty){
-                    item = ((MaterialProperty)property).getItem(player, menu);
-                    break;
-                }
-            }
-
-            if (item == null){
-                throw new IllegalArgumentException("Item has not any material data");
-            }
-
-            // Apply simple properties
-            for (ItemProperty property : properties.values()){
-                if (property instanceof SimpleProperty){
-                    ItemMeta meta = item.getItemMeta();
-                    Optional<ItemStack> stack = ((SimpleProperty)property).apply(item, meta, player, menu);
-
-                    if (stack.isPresent()){
-                        item = stack.get();
-                    } else {
-                        item.setItemMeta(meta);
-                    }
-                }
-            }
-
-            return item;
-        } catch (Exception e){
-            Logger.warning("Error while building ItemStack: " + e.getMessage());
-        }
-
-        return null;
-    }
+    ItemProperty removeProperty(String key);
 
     /**
      * Check is this item similar to som ItemStack
@@ -104,37 +37,13 @@ public class Item implements Cloneable {
      * @param player Player to replace placeholders
      * @return true if this item similar to provided or false otherwise
      */
-    public boolean isSimilar(ItemStack item, Player player){
-        if (item == null) return false;
-        return item.isSimilar(build(player, null));
-    }
+    boolean isSimilar(ItemStack item, Player player);
 
-    @Override
-    public Item clone() {
-        try{
-            Item item = (Item) super.clone();
-            item.properties = new HashMap<>(item.properties);
-            return item;
-        } catch (CloneNotSupportedException e){
-            return null;
-        }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o instanceof Item){
-            return Objects.equals(this.properties, ((Item) o).properties);
-        }
-        return false;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(this.properties);
-    }
-
-    @Override
-    public String toString(){
-        return "Item:{properties: "+properties+"}";
-    }
+    /**
+     * Build ItemStack of this item
+     * @param player Player to correct replace all placeholders
+     * @param menu Menu in which this item exists. Might be null
+     * @return Built ItemStack object
+     */
+    ItemStack build(Player player, Menu menu);
 }
--- src/main/java/ru/nanit/abstractmenus/api/inventory/property/ItemProperty.java
+++ src/main/java/ru/nanit/abstractmenus/api/inventory/property/ItemProperty.java
@@ -1,8 +1,24 @@
 package ru.nanit.abstractmenus.api.inventory.property;
 
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import ru.nanit.abstractmenus.api.inventory.Menu;
+
 /**
  * Represent any item property
  */
 public interface ItemProperty {
+
+    /**
+     * Apply property to ItemStack
+     * @param item Source ItemStack
+     * @param meta Current meta of this item
+     * @param player Player for who this item builds
+     * @param menu Menu which cause item building
+     * @return true if modified ItemMeta should be applied to item after exiting this method, or false if item
+     * modified manually and saved ItemMeta shouldn't be assigned after exiting this method
+     */
+    boolean apply(ItemStack item, ItemMeta meta, Player player, Menu menu);
 
 }
